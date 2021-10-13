95a4c722b62bf3369ef8c39f2feb7f2d07f05a6c
Patrick <pat_pi@yahoo.com>
2021-10-13 21:41:32 +0200

    custom user and tiers admin panel

--- services/backend/tiers/admin.py
+++ services/backend/tiers/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from tiers.models import Tier, Size
+from tiers.models import Size, Tier
 
-admin.site.register(Tier)
-admin.site.register(Size)
+
+class TierDashboard(admin.ModelAdmin):
+    list_display = ('name', 'link_flag', 'expired_link_flag')
+    list_editable = ('link_flag', 'expired_link_flag')
+
+
+class SizeDashboard(admin.ModelAdmin):
+    list_display = ('id', 'height')
+    list_editable = ('height', )
+
+
+admin.site.register(Size, SizeDashboard)
+admin.site.register(Tier, TierDashboard)
--- services/backend/users/admin.py
+++ services/backend/users/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import CustomUser
 
-admin.site.register(CustomUser)
+
+class CustomUserDashboard(UserAdmin):
+    list_display = ('id', 'username', 'tier')
+    list_editable = ('tier', )
+    list_display_links = ('id', 'username')
+    ordering = ('-id',)
+
+
+admin.site.register(CustomUser, CustomUserDashboard)
+
+admin.site.site_header = 'Admin Panel'
+admin.site.site_title = 'Image Resizer'
+admin.site.index_title = 'Administration'
