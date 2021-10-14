0d34ae3071590cb30a5c7871db26bb5a638b2497
Patrick <pat_pi@yahoo.com>
2021-10-14 13:58:45 +0200

    helper function for creating thumbnails

---
+++ services/backend/helpers/create_thumbnail.py
@@ -0,0 +1,21 @@
+import os
+
+import PIL
+from PIL import Image
+from django.conf import settings
+
+
+def create_thumbnail(image, height):
+    img = Image.open(image.url)
+    h_percent = (height / float(img.size[1]))
+    width = int((float(img.size[0]) * float(h_percent)))
+    img = img.resize((width, height), PIL.Image.LANCZOS)
+
+    path = settings.MEDIA_ROOT + 'resized_images'
+    os.makedirs(path, exist_ok=True)
+
+    filename = image.name
+    new_filename = os.path.splitext(filename)[0] + '_' + str(height) + os.path.splitext(filename)[1]
+    file_path = path + os.path.sep + new_filename
+    img.save(file_path)
+    return new_filename
