f00e57fed294c0d58923535f01adbc0d580c0328
Patrick <pat_pi@yahoo.com>
2021-10-14 15:27:48 +0200

    redirect from main page

--- services/backend/config/urls.py
+++ services/backend/config/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='api/v1/images')),
     path('api/v1/', include('images.urls')),
 ]
 
